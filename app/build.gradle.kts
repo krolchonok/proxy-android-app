9a4636b58aa47c62e3aa1768ef9a548338e2c944
ushastoe <fedosey8@gmail.com>
2024-02-19 13:58:47 +0600

    hehe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,9 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("com.scottyab:rootbeer-lib:0.1.0")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/ushastoe/proxy/MainActivity.kt
+++ app/src/main/java/com/ushastoe/proxy/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ushastoe.proxy
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.ContextThemeWrapper
-import android.view.Gravity
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.EditText
@@ -13,79 +12,73 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.color.DynamicColors
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity() {
-    private val name_setting = "prefs_proxy"
+    private val namesetting = "prefs_proxy"
 
     @SuppressLint("ResourceType", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        DynamicColors.applyToActivitiesIfAvailable(application)
 
         registercallback()
 
-        val textView = findViewById<TextView>(R.id.infotext)
-        textView.text = checkproxy()
-        textView.gravity = Gravity.CENTER
+        findViewById<TextView>(R.id.infotext).text = checkproxy()
         val list = getListSaveProxy()?.toMutableList()
-        generate_button(list ?: return)
-        val savebutton = findViewById<Button>(R.id.save)
-        savebutton.setOnClickListener {
+        generateButton(list ?: return)
+
+         findViewById<Button>(R.id.save).setOnClickListener {
             if (findViewById<EditText>(R.id.proxytext).text.toString() in list) {
                 Toast.makeText(this, "IP уже был сохранен", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             } else {
                 Toast.makeText(this, "IP сохранен", Toast.LENGTH_SHORT).show()
                 list += findViewById<EditText>(R.id.proxytext).text.toString()
-                generate_button(list)
+                generateButton(list)
                 saveListProxy(list)
             }
         }
-        val clearbutton = findViewById<Button>(R.id.clear)
-        clearbutton.setOnClickListener {
+        findViewById<Button>(R.id.clear).setOnClickListener {
             list.clear()
             list += "10.0.0.10"
-            generate_button(list)
+            generateButton(list)
             Toast.makeText(this, "Список очищен", Toast.LENGTH_SHORT).show()
             saveListProxy(list)
         }
     }
 
 
-    private fun generate_button(list: List<String>) {
-        val existingLinearLayout = findViewById<LinearLayout>(R.id.ip_layout)
-        existingLinearLayout.removeAllViews()
+    private fun generateButton(list: List<String>) {
+    findViewById<LinearLayout>(R.id.ip_layout).removeAllViews()
+
 
         val themeWrapper = ContextThemeWrapper(
             this,
             R.style.MyButton
         )
         for (i in list) {
-
-
             val button = MaterialButton(themeWrapper)
             button.text = i
             button.setOnClickListener {
                 findViewById<EditText>(R.id.proxytext).setText(i)
                 findViewById<EditText>(R.id.proxytext).setSelection(findViewById<EditText>(R.id.proxytext).length())
             }
-            existingLinearLayout.addView(button)
+            findViewById<LinearLayout>(R.id.ip_layout).addView(button)
         }
     }
     private fun saveListProxy(list: List<String>) {
-        val preferences = getSharedPreferences(name_setting, MODE_PRIVATE)
+        val preferences = getSharedPreferences(namesetting, MODE_PRIVATE)
         val editor = preferences.edit()
         editor.putString("proxy_last", list.joinToString(";"))
         editor.apply()
-
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -95,10 +88,8 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.infotext)
 
         inputtext.setOnTouchListener { _, event ->
-            val DRAWABLE_RIGHT = 2
-
             if (event.action == MotionEvent.ACTION_UP) {
-                if (event.rawX >= (inputtext.right - inputtext.compoundDrawables[DRAWABLE_RIGHT].bounds.width())) {
+                if (event.rawX >= (inputtext.right - inputtext.compoundDrawables[2].bounds.width())) {
                     inputtext.text = null
                     return@setOnTouchListener true
                 }
@@ -115,35 +106,26 @@ class MainActivity : AppCompatActivity() {
         buttonsend.setOnClickListener {
             if (checkportinproxy(inputtext.text.toString())) {
                 runCommand("su -c settings put global http_proxy ${inputtext.text}")
-                repeat(5) {
-                    textView.text = checkproxy()
-                    Thread.sleep(50)
-                }
+                Thread.sleep(50)
+                textView.text = checkproxy()
             } else {
                 Toast.makeText(this, "Прокси без порта", Toast.LENGTH_SHORT).show()
             }
         }
 
-        val button3128 = findViewById<Button>(R.id.button1)
-        button3128.setOnClickListener {
-            val newip = inputtext.text.toString()
-            val modifiedAddress = replacePort(newip, 3128)
+
+        findViewById<Button>(R.id.b3128).setOnClickListener {
+            val modifiedAddress = replacePort(inputtext.text.toString(), 3128)
             inputtext.setText(modifiedAddress)
             inputtext.setSelection(inputtext.length())
         }
 
-        val button80 = findViewById<Button>(R.id.button2)
-
-        button80.setOnClickListener {
-            val newip = inputtext.text.toString()
-            val modifiedAddress = replacePort(newip, 80)
+        findViewById<Button>(R.id.b80).setOnClickListener {
+            val modifiedAddress = replacePort(inputtext.text.toString(), 80)
             inputtext.setText(modifiedAddress)
             inputtext.setSelection(inputtext.length())
         }
-
-        val buttonempty = findViewById<Button>(R.id.button3)
-
-        buttonempty.setOnClickListener {
+        findViewById<Button>(R.id.bclear).setOnClickListener {
             inputtext.setText(":0")
             inputtext.setSelection(inputtext.length())
         }
@@ -179,7 +161,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getListSaveProxy(): List<String>? {
-        val preferences = getSharedPreferences(name_setting, MODE_PRIVATE)
+        val preferences = getSharedPreferences(namesetting, MODE_PRIVATE)
         val savedText = preferences.getString("proxy_last", "10.0.0.10")
         if (savedText != null) {
             return savedText.split(";")
